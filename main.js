80c97d4ca34be4736e17d7f72ddc09618ac4c489
saworieza <saworieza@gmail.com>
2022-07-09 05:40:19 +0300

    understanding constructors

---
+++ main.js
@@ -0,0 +1,14 @@
+// Day One - Constructors
+function User(name, age,race) {
+    this.name = name;
+    this.age = age;
+    this.race = race;
+}
+
+let User1 = new User('Steve', 32, 'African');
+let User2 = new User('Stella', 33, 'European');
+let User3 = new User('George', 324, 'Asian');
+
+console.log(User1)
+console.log(User1)
+console.log(User3)
