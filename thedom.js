841d764e6e3202a27cfd243d65562f0359360713
Saworieza <saworieza@gmail.com>
2022-11-09 15:58:59 +0300

    traversing the dom

---
+++ thedom.js
@@ -0,0 +1,12 @@
+// Master This 10 and we can traverse the DOM freely
+// 1. getElementById
+// 2. getElementsByClassName
+// 3. QuerySelector
+// 4. querySelectorAll
+// 5. Selecting Children
+// 6. Selecting Descendants
+// 7. Selecting Parents
+// 8. Selecting Ancestors
+// 9. nextElementSibling
+//10. previousElementSibling
+
