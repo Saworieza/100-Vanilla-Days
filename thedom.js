b7c625056c2d36ab6850e82279a60362ed5ecac4
Saworieza <saworieza@gmail.com>
2022-11-09 23:01:27 +0300

    class and id selectors understood

--- thedom.js
+++ thedom.js
@@ -10,3 +10,17 @@
 // 9. nextElementSibling
 //10. previousElementSibling
 
+//Get Element By Id. Can only ever select one as only one id is proper
+const grandparent = document.getElementById('grandparent-id');
+//get Element by Class allows for multiple selections using array methods
+const parents = Array.from(document.getElementsByClassName("parent"))
+
+// grandparent.style.backgroundColor = "#333"
+// create a fucntion to house the change color just in case there are multiple opportunities to share
+// changeColor(grandparent);
+//loop through each class
+parents.forEach(changeColor);
+
+function changeColor(el){
+  el.style.backgroundColor = "#333"
+}
